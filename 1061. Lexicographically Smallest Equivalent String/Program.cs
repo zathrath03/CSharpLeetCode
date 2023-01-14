680d967c2e6febd37f8caf288cb6c10284ecac35
J D <zathrath03@gmail.com>
2023-01-13 19:47:43 -0800

    Solution

---
+++ 1061. Lexicographically Smallest Equivalent String/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace _1061.Lexicographically_Smallest_Equivalent_String
+{
+    // You are given two strings of the same length s1 and s2 and a string baseStr.
+
+    // We say s1[i] and s2[i] are equivalent characters.
+
+    // For example, if s1 = "abc" and s2 = "cde", then we have 'a' == 'c', 'b' == 'd', and 'c' == 'e'.
+    // Equivalent characters follow the usual rules of any equivalence relation:
+
+    // Reflexivity: 'a' == 'a'.
+    // Symmetry: 'a' == 'b' implies 'b' == 'a'.
+    // Transitivity: 'a' == 'b' and 'b' == 'c' implies 'a' == 'c'.
+    // For example, given the equivalency information from s1 = "abc" and s2 = "cde", "acd" and "aab" are equivalent strings of baseStr = "eed", and "aab" is the lexicographically smallest equivalent string of baseStr.
+
+    // Return the lexicographically smallest equivalent string of baseStr by using the equivalency information from s1 and s2.
+    internal class Program
+    {
+        private static void Main()
+        {
+            var testCases = new List<(string, string, string, string)>
+            {
+                ("parker", "morris", "parser", "makkek"),
+                ("hello", "world", "hold", "hdld"),
+                ("leetcode", "programs", "sourcecode", "aauaaaaada")
+            };
+
+            var sol = new Solution();
+            foreach (var (s1, s2, baseStr, expected) in testCases)
+            {
+                var result = sol.SmallestEquivalentString(s1, s2, baseStr);
+                Console.WriteLine(
+                    $"SmallestEquivalentString({s1}, {s2}, {baseStr}) = {result} when {expected} was expected");
+                Debug.Assert(result == expected);
+            }
+        }
+    }
+}
---
+++ 1061. Lexicographically Smallest Equivalent String/Solution.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace _1061.Lexicographically_Smallest_Equivalent_String
+{
+    public class Solution
+    {
+        private static Dictionary<char, HashSet<char>> _charMap;
+        private static Dictionary<char, char> _memo;
+
+        public string SmallestEquivalentString(string s1, string s2, string baseStr)
+        {
+            _charMap = new Dictionary<char, HashSet<char>>();
+            _memo = new Dictionary<char, char>();
+
+            foreach (var (c1, c2) in s1.Zip(s2, (c1, c2) => (first: c1, second: c2)))
+            {
+                AddToMap(c1, c2);
+                AddToMap(c2, c1);
+            }
+
+            return string.Join("", baseStr.Select(Bfs));
+        }
+
+        private static void AddToMap(char key, char val)
+        {
+            if (_charMap.ContainsKey(key))
+                _charMap[key].Add(val);
+            else
+                _charMap[key] = new HashSet<char> { val };
+        }
+
+        private static char Bfs(char c)
+        {
+            if (_memo.ContainsKey(c)) return _memo[c];
+            if (!_charMap.ContainsKey(c)) return c;
+
+            var smallestChar = FindSmallestChar(c, out var seen);
+
+            UpdateMemo(seen, smallestChar);
+
+            return smallestChar;
+        }
+
+        private static char FindSmallestChar(char c, out HashSet<char> seen)
+        {
+            var smallestChar = c;
+            seen = new HashSet<char>();
+            var queue = new HashSet<char> { c };
+
+            smallestChar = SmallestCharLoop(seen, queue, smallestChar);
+
+            return smallestChar;
+        }
+
+        private static void UpdateMemo(HashSet<char> seen, char smallestChar)
+        {
+            foreach (var c in seen) _memo[c] = smallestChar;
+        }
+
+        private static char SmallestCharLoop(ISet<char> seen, ISet<char> queue, char smallestChar)
+        {
+            while (queue.Count > 0)
+            {
+                var c = Pop(queue);
+                if (seen.Contains(c)) continue;
+                seen.Add(c);
+                smallestChar = smallestChar.CompareTo(c) < 0 ? smallestChar : c;
+                queue.UnionWith(_charMap[c]);
+            }
+
+            return smallestChar;
+        }
+
+        private static char Pop(ICollection<char> queue)
+        {
+            var c = queue.First();
+            queue.Remove(c);
+            return c;
+        }
+    }
+}
