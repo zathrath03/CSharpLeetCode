08b63472389e7c56b63933ce7a9806e89b07bfe6
J D <zathrath03@gmail.com>
2023-01-14 19:54:38 -0800

    Full implementation

---
+++ 2421NumberOfGoodPaths/Program.cs
@@ -0,0 +1,51 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+namespace _2421NumberOfGoodPaths;
+
+public class Solution
+{
+    public static int NumberOfGoodPaths(int[] vals, int[][] edges)
+    {
+        var sortedEdges = edges.OrderBy(x => Math.Max(vals[x[0]], vals[x[1]]));
+        var n = vals.Length;
+        var goodPaths = n;
+        var par = Enumerable.Range(0, n).ToArray();
+        var rank = Enumerable.Repeat(1, n).ToArray();
+
+        foreach (var edge in sortedEdges)
+        {
+            var a = edge[0];
+            var b = edge[1];
+            var parentA = FindParent(a, par);
+            var parentB = FindParent(b, par);
+
+            if (vals[parentA] == vals[parentB])
+            {
+                goodPaths += rank[parentA] * rank[parentB];
+                par[parentA] = parentB;
+                rank[parentB] += rank[parentA];
+            }
+            else if (vals[parentA] > vals[parentB])
+            {
+                par[parentB] = parentA;
+            }
+            else
+            {
+                par[parentA] = parentB;
+            }
+        }
+
+        return goodPaths;
+    }
+
+    private static int FindParent(int p, IList<int> par)
+    {
+        while (par[p] != p)
+        {
+            par[p] = par[par[p]];
+            p = par[p];
+        }
+
+        return p;
+    }
+}
