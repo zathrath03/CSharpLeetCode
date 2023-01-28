5fbcf36e3d42c79b2d72c3d3c3f8125420142c12
J D <zathrath03@gmail.com>
2023-01-28 09:10:12 -0800

    Implement solution

--- _352DataStreamAsDisjointIntervals.cs/SummaryRanges.cs
+++ _352DataStreamAsDisjointIntervals.cs/SummaryRanges.cs
@@ -1,22 +1,44 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace _352DataStreamAsDisjointIntervals.cs
 {
     public class SummaryRanges
     {
+        private readonly SortedSet<int> _integersSeen;
+
         public SummaryRanges()
         {
-            
+            _integersSeen = new SortedSet<int>();
         }
 
         public void AddNum(int value)
         {
-            
+            _integersSeen.Add(value);
         }
 
         public int[][] GetIntervals()
         {
-            return Array.Empty<int[]>();
+            if (_integersSeen.Count == 0) return Array.Empty<int[]>();
+            
+            var intervals = new List<int[]>();
+            int left = -1, right = -1;
+
+            foreach (var num in _integersSeen)
+            {
+                if (left < 0)
+                    left = right = num;
+                else if (num == right + 1)
+                    right = num;
+                else
+                {
+                    intervals.Add(new int[] {left, right});
+                    left = right = num;
+                }
+            }
+            intervals.Add(new int[] {left, right});
+            return intervals.ToArray();
         }
     }
 }
