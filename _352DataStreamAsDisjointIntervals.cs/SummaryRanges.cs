5feae788167ff7e77cca0c4eb1d1ffee3a6ea563
J D <zathrath03@gmail.com>
2023-01-28 08:31:01 -0800

    Initial problem setup

---
+++ _352DataStreamAsDisjointIntervals.cs/SummaryRanges.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _352DataStreamAsDisjointIntervals.cs
+{
+    public class SummaryRanges
+    {
+        public SummaryRanges()
+        {
+            
+        }
+
+        public void AddNum(int value)
+        {
+            
+        }
+
+        public int[][] GetIntervals()
+        {
+            return Array.Empty<int[]>();
+        }
+    }
+}
